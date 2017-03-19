afa6d0344c97edb8fc0adbf19d0395b20adf3faa
ebadia <enric.badia@gmail.com>
2017-03-19 18:54:20 +0100

    localdb

--- config/models.js
+++ config/models.js
@@ -17,8 +17,9 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-  connection: 'mysqlDb',
-
+  //connection: 'mysqlDb',
+  connection: 'localDiskDb',
+  
   /***************************************************************************
   *                                                                          *
   * How and whether Sails will attempt to automatically rebuild the          *
